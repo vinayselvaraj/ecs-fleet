ce760a9c286d62de0ce86e5c8eff8bdde5357bc7
Vinay Selvaraj <vinay@selvaraj.com>
2019-08-15 14:55:07 -0700

    Setting subnet config for just public subnets

--- src/main/java/ecsfleet/AppStack.java
+++ src/main/java/ecsfleet/AppStack.java
@@ -3,8 +3,12 @@ package ecsfleet;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ec2.VpcProps;
+
+import java.util.Arrays;
 
 public class AppStack extends Stack {
     public AppStack(final Construct parent, final String id) {
@@ -15,7 +19,10 @@ public class AppStack extends Stack {
         super(parent, id, props);
 
         new Vpc(this, "ECS Fleet VPC", VpcProps.builder()
-                .withNatGateways(0)
+                .withSubnetConfiguration(
+                        Arrays.asList(SubnetConfiguration.builder()
+                                .withSubnetType(SubnetType.PUBLIC)
+                                .build()))
                 .build());
     }
 }
