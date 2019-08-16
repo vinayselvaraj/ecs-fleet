37c1abc4763be218f6249e333dba13033e52813d
Vinay Selvaraj <vinay@selvaraj.com>
2019-08-15 17:12:43 -0700

    Added AutoScaling Group

--- src/main/java/ecsfleet/AppStack.java
+++ src/main/java/ecsfleet/AppStack.java
@@ -3,16 +3,18 @@ package ecsfleet;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.ec2.SubnetConfiguration;
-import software.amazon.awscdk.services.ec2.SubnetType;
-import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ec2.VpcProps;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroupProps;
+import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ClusterProps;
+import software.amazon.awscdk.services.ecs.EcsOptimizedAmi;
+import software.amazon.awscdk.services.ssm.StringParameter;
 
 import java.util.Arrays;
 
 public class AppStack extends Stack {
+
     public AppStack(final Construct parent, final String id) {
         this(parent, id, null);
     }
@@ -33,6 +35,17 @@ public class AppStack extends Stack {
         // Create ECS Cluster
         new Cluster(this, "ECS Fleet Cluster", ClusterProps.builder()
                 .withVpc(vpc)
+                .build());
+
+        new AutoScalingGroup(this, "ECS Fleet ASG", AutoScalingGroupProps.builder()
+                .withMachineImage(new EcsOptimizedAmi())
+                .withInstanceType(InstanceType.of(
+                        InstanceClass.BURSTABLE3,
+                        InstanceSize.NANO))
+                .withVpc(vpc)
+                .withVpcSubnets(SubnetSelection.builder()
+                        .withSubnetType(SubnetType.PUBLIC)
+                        .build())
                 .build());
     }
 }
